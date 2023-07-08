edb8167a6a31e4f393c42eb09dd7f80a06e8e8e3
eric-f-souza <eric.vash@hotmail.com>
2023-07-08 14:59:21 -0300

    atualizações

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -1,5 +1,10 @@
 ﻿
-List<string> listaBandas = new List<string>();
+//List<string> listaBandas = new List<string> {"U2", "Beatles", "AC/DC" };
+Dictionary<string,List<int>> listaBandasRegistradas = new Dictionary<string,List<int>>();
+listaBandasRegistradas.Add("Linkin park", new List<int>{10, 9, 9});
+listaBandasRegistradas.Add("U2", new List<int> { 8, 7, 8});
+listaBandasRegistradas.Add("Skillet", new List<int>());
+
 void ExibirLogo()
 {
     Console.WriteLine(@"
@@ -22,9 +27,9 @@ void Menu()
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média de uma banda");
-    Console.WriteLine("Digite -1 para sair");
+    Console.WriteLine("Digite -1 para sair\n");
 
-    Console.WriteLine("\nDigite a sua opção: ");
+    Console.WriteLine("Digite a sua opção: ");
     String opcaoEscolhida = Console.ReadLine()!;
 
     int opcaoEscolhidaNumerico = int.Parse(opcaoEscolhida);
@@ -35,7 +40,7 @@ void Menu()
             break;
         case 2: MostrarBandas();
             break;
-        case 3: Console.WriteLine("Você digitou a opção " + opcaoEscolhidaNumerico);
+        case 3: AvaliarBanda();
             break;
         case 4:Console.WriteLine("Você digitou a opção " + opcaoEscolhidaNumerico);
             break;
@@ -46,28 +51,71 @@ void Menu()
     }
 }
 
-void MostrarBandas()
-{
-    for (int i = 0; i < listaBandas.Count; i++)
-    {
-        Console.WriteLine($"Banda: {listaBandas[i]}");
-    }
-    Console.WriteLine("Precione qualquer tecla para retornar ao menu");
-    Console.ReadLine();
-    Menu();
-}
-
 void RegistrarBanda()
 {
     Console.Clear();
-    Console.WriteLine("Registro de bandas");
+    Banerfuncao("Registrar banda");
     Console.Write("Digite o nome da banda que deseja registrar: ");
     string nomeDaBanda = Console.ReadLine()!;
-    listaBandas.Add(nomeDaBanda);
-    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+    listaBandasRegistradas.Add(nomeDaBanda, new List<int>());
+    Console.WriteLine($"\nA banda {nomeDaBanda} foi registrada com sucesso!");
+    Console.WriteLine($"\nPrecione qualquer tecla para retornar ao menu");
+    Console.ReadKey();
+    Console.Clear();
     Menu();
 }
 
+void MostrarBandas()
+{
+    Console.Clear();
+    Banerfuncao("Lista bandas cadastradas");
+ //   for (int i = 0; i < listaBandas.Count; i++)
+ //   {
+ //       Console.WriteLine($"Banda: {listaBandas[i]}");
+ //   }
+     foreach (var banda in listaBandasRegistradas.Keys)
+     {
+         Console.WriteLine($"Banda: {banda}");
+     }
+    Console.WriteLine("\nPrecione qualquer tecla para retornar ao menu");
+    Console.ReadKey();
+    Console.Clear();
+    Menu();
+}
 
+void Banerfuncao(string mensagem)
+{
+    string asteriscos = string.Empty.PadLeft(mensagem.Length, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(mensagem);
+    Console.WriteLine(asteriscos + "\n");
+}
 
+void AvaliarBanda()
+{
+    //validar se a banda esta cadastrada
+    Console.Clear();
+    Banerfuncao("Avaliar banda");
+    Console.WriteLine("Digite a banda que deseja valiar\n");
+    string banda  = Console.ReadLine()!;
+    if (listaBandasRegistradas.ContainsKey(banda))
+    {
+        Console.WriteLine($"Avalie a banda {banda}, com um valor de 1 a 10: ");
+        int nota = int.Parse(Console.ReadLine()!);
+        listaBandasRegistradas[banda].Add(nota);
+        Console.WriteLine($"\nA banda {banda}, recebeu a nota {nota}");
+        Thread.Sleep(4000);
+        Console.Clear();
+        Menu();
+    }
+    else
+    {
+        Console.WriteLine($"A banda {banda} não esta registrada\n");
+        Console.WriteLine("Digite uma tecla para voltar para o menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        Menu();
+    }
+    
+}
 Menu();
